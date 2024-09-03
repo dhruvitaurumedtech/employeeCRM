fbcb0ad6f8a6d9c04ce2ba7bbff07a8308b74ee2
dhruvitaurumedtech <162576013+dhruvitaurumedtech@users.noreply.github.com>
2024-09-03 17:29:41 +0530

    c

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -102,51 +102,11 @@
         <ul class="menu-inner py-1">
           <!-- Dashboards -->
           <li class="menu-item ">
-            <a href="javascript:void(0);" class="menu-link menu-toggle">
+            <a href="{{url('/dashboard')}}" class="menu-link ">
               <i class="menu-icon tf-icons bx bx-home-smile"></i>
-              <div class="text-truncate" data-i18n="Dashboards">Dashboards</div>
-              <span class="badge rounded-pill bg-danger ms-auto">5</span>
+              <div class="text-truncate" >Dashboards</div>
             </a>
-            <ul class="menu-sub">
-              <li class="menu-item active">
-                <a href="index.html" class="menu-link">
-                  <div class="text-truncate" data-i18n="Analytics">Analytics</div>
-                </a>
-              </li>
-              <li class="menu-item">
-                <a
-                  href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/html/vertical-menu-template/dashboards-crm.html"
-                  target="_blank"
-                  class="menu-link">
-                  <div class="text-truncate" data-i18n="CRM">CRM</div>
-                  <div class="badge rounded-pill bg-label-primary text-uppercase fs-tiny ms-auto">Pro</div>
-                </a>
-              </li>
-              <li class="menu-item">
-                <a
-                  href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/html/vertical-menu-template/app-ecommerce-dashboard.html"
-                  target="_blank"
-                  class="menu-link">
-                  <div class="text-truncate" data-i18n="eCommerce">eCommerce</div>
-                  <div class="badge rounded-pill bg-label-primary text-uppercase fs-tiny ms-auto">Pro</div>
-                </a>
-              </li>
-              <li class="menu-item">
-                <a
-                  href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/html/vertical-menu-template/app-logistics-dashboard.html"
-                  target="_blank"
-                  class="menu-link">
-                  <div class="text-truncate" data-i18n="Logistics">Logistics</div>
-                  <div class="badge rounded-pill bg-label-primary text-uppercase fs-tiny ms-auto">Pro</div>
-                </a>
-              </li>
-              <li class="menu-item">
-                <a href="app-academy-dashboard.html" target="_blank" class="menu-link">
-                  <div class="text-truncate" data-i18n="Academy">Academy</div>
-                  <div class="badge rounded-pill bg-label-primary text-uppercase fs-tiny ms-auto">Pro</div>
-                </a>
-              </li>
-            </ul>
+           
           </li>
 
           <!-- Layouts -->
